7651f82de0eb50ba9f6ac53d54bead71bfa2697d
Sain-Biswas <sain.biswas614@gmail.com>
2026-08-03 14:28:23 +0530

    feat: make the hero section of exam details page

--- src/app/[exam]/page.tsx
+++ src/app/[exam]/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import { getExaminationDetails } from "~/server/actions/get-examination-details";
+import { Avatar, AvatarFallback, AvatarImage } from "~/shadcn/ui/avatar";
 import {
 	Breadcrumb,
 	BreadcrumbItem,
@@ -9,6 +10,7 @@ import {
 	BreadcrumbPage,
 	BreadcrumbSeparator
 } from "~/shadcn/ui/breadcrumb";
+import { Skeleton } from "~/shadcn/ui/skeleton";
 
 export default async function ExaminationPage({
 	params
@@ -35,7 +37,34 @@ export default async function ExaminationPage({
 					</BreadcrumbList>
 				</Breadcrumb>
 			</header>
-			<main></main>
+			<main>
+				<section className="m-6 flex flex-col items-center gap-6 md:flex-row">
+					<Avatar className="size-30 rounded-none after:content-none">
+						<AvatarImage
+							src={examination.image ?? null}
+							alt={examination.code}
+							className="size-30 rounded-none object-contain!"
+						/>
+						<AvatarFallback
+							render={<Skeleton />}
+							className="size-30 rounded-none"
+						/>
+					</Avatar>
+					<article className="w-full text-center md:text-left">
+						<div className="flex flex-col flex-wrap items-center justify-between md:flex-row">
+							<h1 className="text-3xl/relaxed font-extrabold uppercase">
+								{examination.name}
+							</h1>
+							<h3 className="text-xl/relaxed font-semibold text-muted-foreground uppercase">
+								{examination.code}
+							</h3>
+						</div>
+						<p className="text-base/relaxed font-medium whitespace-pre-wrap text-muted-foreground">
+							{examination.description}
+						</p>
+					</article>
+				</section>
+			</main>
 		</>
 	);
 }
